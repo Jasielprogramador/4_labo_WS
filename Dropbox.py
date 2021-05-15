ecd8b6931a4af65fe5ead1d502ad4fc9f3813414
arrosa <asierrosa@gmail.com>
2021-05-15 19:55:08 +0200

    amaituta

--- Dropbox.py
+++ Dropbox.py
@@ -33,7 +33,7 @@ class Dropbox:
         client_connection, client_address = server_socket.accept()
         eskaera = client_connection.recv(1024)
         print("\tRequest from the browser received at local server:")
-        print (eskaera)
+        print("Eskaera: " + str(eskaera))
 
         # eskaeran "auth_code"-a bilatu
         lehenengo_lerroa = eskaera.decode("utf8").split('\n')[0]
@@ -52,7 +52,6 @@ class Dropbox:
         server_socket.close()
 
         return auth_code
-
 
     def do_oauth(self):
         auth_uri = "https://www.dropbox.com/oauth2/authorize"
@@ -81,7 +80,7 @@ class Dropbox:
         print("\tCotenido:")
         print(contenido)
         contenido_json = json.loads(contenido)
-        access_token =contenido_json['access_token']
+        access_token = contenido_json['access_token']
         print("\taccess_token: " + access_token)
         self._access_token = access_token
 
@@ -89,36 +88,36 @@ class Dropbox:
 
     def list_folder(self, msg_listbox):
         print("/list_folder")
-        uri = "https://api.dropboxapi.com/2/files/list_folder"
-        if (self._path == '/'):
-            path = ""
-        else:
-            path = self._path
-        datuak = {'path': path, "recursive": False, "include_media_info": False, "include_deleted": False,
-                  "include_has_explicit_shared_members": False, "include_mounted_folders": True,
-                  "include_non_downloadable_files": True}
-        datuak_encoded = json.dumps(datuak)
-        goiburuak = {'Host': 'api.dropboxapi.com', 'Authorization': 'Bearer ' + self._access_token,
+        if self._path == "/":
+            self._path = ""
+        list_uri = 'https://api.dropboxapi.com/2/files/list_folder'
+        cabeceras = {'Authorization': 'Bearer ' + self._access_token,
                      'Content-Type': 'application/json'}
-        erantzuna = requests.post(uri, headers=goiburuak, data=datuak_encoded, allow_redirects=False)
-        status = erantzuna.status_code
-        print("\tEdukia:")
-        edukia = erantzuna.text
-        print(edukia)
-        edukia_json_dict = json.loads(edukia)
+        datos = {"path": self._path, "recursive": False, "include_media_info": False, "include_deleted": False,
+                 "include_has_explicit_shared_members": False, "include_mounted_folders": True,
+                 "include_non_downloadable_files": True}
+        datos_encoded = json.dumps(datos)
+        respuesta = requests.post(list_uri, headers=cabeceras, data=datos_encoded, allow_redirects=False)
+        status = respuesta.status_code
+        print("\tStatus: " + str(status))
+        contenido = respuesta.text
+        print("\tContenido:")
+        print(contenido)
+        edukia_json_dict = json.loads(contenido)
 
         self._files = helper.update_listbox2(msg_listbox, self._path, edukia_json_dict)
 
     def transfer_file(self, file_path, file_data):
-        print("/upload" + file_path)
+        print("/upload " + file_path)
         uri = "https://content.dropboxapi.com/2/files/upload"
         datuak = {'path': file_path, 'mode': 'add', 'autorename': True, 'mute': False, 'strict_conflict': False}
         datuak_json = json.dumps(datuak)
+
         goiburuak = {'Host': 'content.dropboxapi.com', 'Authorization': 'Bearer ' + self._access_token,
                      'Dropbox-API-Arg': datuak_json, 'Content-Type': 'application/octet-stream'}
         erantzuna = requests.post(uri, headers=goiburuak, data=file_data, allow_redirects=False)
         status = erantzuna.status_code
-
+        print("\nStatus:" + str(status))
 
     def delete_file(self, file_path):
         print("/delete_file " + file_path)
@@ -133,6 +132,7 @@ class Dropbox:
 
     def create_folder(self, path):
         print("/create_folder " + path)
+        print("/create_folder " + str(path))
         uri = 'https://api.dropboxapi.com/2/files/create_folder'
         datuak = {'path': path, 'autorename': False}
         datuak_json = json.dumps(datuak)
@@ -140,4 +140,3 @@ class Dropbox:
                      'Content-Type': 'application/json'}
         erantzuna = requests.post(uri, headers=goiburuak, data=datuak_json, allow_redirects=False)
         status = erantzuna.status_code
-
--- eGela.py
+++ eGela.py
@@ -6,7 +6,6 @@ from bs4 import BeautifulSoup
 import time
 import helper
 
-
 class eGela:
     _login = 0
     _cookie = ""
@@ -17,32 +16,33 @@ class eGela:
         self._root = root
 
     def check_credentials(self, username, password, event=None):
+
+        cookiea = ""
         popup, progress_var, progress_bar = helper.progress("check_credentials", "Logging into eGela...")
         progress = 0
         progress_var.set(progress)
         progress_bar.update()
 
         print("##### 1. ESKAERA #####")
-        metodo = 'GET'
-        goiburuak = {'Host': 'egela.ehu.eus'}
-        edukia = ''
+        metodo = "POST"
+        edukia = {'username': username.get(), 'password': password.get()}
+        goiburuak = {'Host': 'egela.ehu.eus', 'Content-Type': 'application/x-www-form-urlencoded',
+                     'Content-Length': str(len(edukia))}
         uria = "http://egela.ehu.eus/login/index.php"
 
-        erantzuna = requests.request(metodo, uria, headers=goiburuak, data=edukia, allow_redirects=False)
+        edukia_encoded = urllib.parse.urlencode(edukia)
+        goiburuak['Content-Length'] = str(len(edukia_encoded))
+
+        erantzuna = requests.request(metodo, uria, headers=goiburuak, data=edukia_encoded, allow_redirects=False)
 
         codigo = erantzuna.status_code
         descripcion = erantzuna.reason
         print(str(codigo) + " " + descripcion)
 
-        cookiea = ""
-        location = ""
-
         if 'Location' in erantzuna.headers:
-            location = erantzuna.headers['Location']
+            uria = erantzuna.headers['Location']
         if 'Set-Cookie' in erantzuna.headers:
             cookiea = erantzuna.headers['Set-Cookie'].split(",")[0]
-
-        self._cookie = cookiea
 
         progress = 33
         progress_var.set(progress)
@@ -54,16 +54,15 @@ class eGela:
         metodo = 'POST'
         goiburuak = {'Host': host, 'Cookie': cookiea,
                      'Content-Type': 'application/x-www-form-urlencoded',
-                     'Content-Length': '0'}
-        edukia = {'username': username, 'password': password}
+                     'Content-Length': str(len(edukia))}
         edukia_encoded = urllib.parse.urlencode(edukia)
         goiburuak['Content-Length'] = str(len(edukia_encoded))
-        erantzuna = requests.request(metodo, location, headers=goiburuak, data=edukia_encoded, allow_redirects=False)
+        erantzuna = requests.request(metodo, uria, headers=goiburuak, data=edukia_encoded, allow_redirects=False)
 
 
 
         print("metodoa: " + metodo)
-        print("uria: " + location)
+        print("uria: " + uria)
         print("edukia: " + edukia_encoded)
 
         codigo = erantzuna.status_code
@@ -71,7 +70,7 @@ class eGela:
         print(str(codigo) + " " + descripcion)
 
         if 'Location' in erantzuna.headers:
-            location = erantzuna.headers['Location']
+            uria= erantzuna.headers['Location']
         if 'Set-Cookie' in erantzuna.headers:
             cookiea = erantzuna.headers['Set-Cookie'].split(",")[0]
 
@@ -83,14 +82,20 @@ class eGela:
         print("\n##### 3. ESKAERA #####")
 
         metodo = 'GET'
-        goiburuak = {'Host': host, 'Cookie': cookiea}
-        edukia = ''
-        erantzuna = requests.request(metodo, location, headers=goiburuak, data=edukia, allow_redirects=False)
+        goiburuak = {'Host': 'egela.ehu.eus', "Cookie": cookiea}
+        erantzuna = requests.request(metodo, uria, headers=goiburuak, allow_redirects=False)
 
         print("HIRUGARREN ESKAERA")
         print("metodoa: " + metodo)
-        print("uria: " + location)
-        print("edukia: " + edukia)
+        print("uria: " + uria)
+        #print("edukia: " + edukia_encoded)
+
+        if 'Location' in erantzuna.headers:
+            uria = erantzuna.headers['Location']
+        if 'Set-Cookie' in erantzuna.headers:
+            cookiea = erantzuna.headers['Set-Cookie'].split(",")[0]
+
+        erantzuna = requests.request(metodo, uria, headers=goiburuak, allow_redirects=False)
 
         codigo = erantzuna.status_code
         descripcion = erantzuna.reason
@@ -102,15 +107,18 @@ class eGela:
         time.sleep(0.1)
         popup.destroy()
 
-        erantzuna.headers['Location'] = 'https://egela.ehu.eus/course/view.php?id=29145'
-        if erantzuna.headers.__contains__('Location'):
-            headers = {'Host': 'egela.ehu.eus', 'Cookie': self._cookie}
-            erantzuna.headers['Location'] = 'https://egela.ehu.eus/course/view.php?id=42336'
-            requests.request('GET', erantzuna.headers['Location'], headers=headers, allow_redirects=False)
+
+
+        COMPROBACION_DE_LOG_IN = erantzuna.status_code == 200
+
+        if COMPROBACION_DE_LOG_IN:
             self._login = 1
+            print(self._login)
+            self._cookie = cookiea
+            #print(self._cookiea)
             self._root.destroy()
         else:
-            print("Alert Message", "Login incorrect!")
+            messagebox.showinfo("Alert Message", "Login incorrect!")
 
 
 
@@ -122,63 +130,74 @@ class eGela:
 
         print("\n##### 4. ESKAERA (Ikasgairen eGelako orrialde nagusia) #####")
 
-        host = "egela.ehu.eus"
-        metodo = 'GET'
-        uria = "https://egela.ehu.eus/course/view.php?id=42336"
-        goiburuak = {'Host': host, 'Cookie': self._cookie}
-        edukia = ''
-        erantzuna = requests.request(metodo, uria, headers=goiburuak, data=edukia, allow_redirects=False)
+        metodo = 'POST'
+        datuak = ""
+        goiburuak = {'Host': 'egela.ehu.eus', 'Content-Type': 'application/x-www-form-urlencoded',
+                     'Content-Length': str(len(datuak)), "Cookie": self._cookie}
+        uria = "https://egela.ehu.eus/course/view.php?id=42336&section=1"
+        erantzuna = requests.request(metodo, uria, headers=goiburuak, allow_redirects=False)
 
         print("LAUGARREN ESKAERA")
 
         print("metodoa: " + metodo)
         print("uria: " + uria)
-        print("edukia: " + edukia)
 
         codigo = erantzuna.status_code
         descripcion = erantzuna.reason
         print(str(codigo) + " " + descripcion)
 
+        if (erantzuna.status_code == 200):
+            print("Web Sistemak")
+            soup = BeautifulSoup(erantzuna.content, "html.parser")
+            pdf_results = soup.find_all("div", {"class": "activityinstance"})
+            kop = str(pdf_results).count("pdf")
+
+        print("PDF kopurua " + str(kop))
+        # print("PDF kopurua " + str(len(self._refs)))
+        progress_step = float(100.0 / kop)
+        # progress_step = float(100.0 / len(self._refs))
         print("\n##### HTML-aren azterketa... #####")
 
-        soup = BeautifulSoup(erantzuna.content, 'html.parser')
-        a_links = soup.find_all("a", {'class': 'ehu-visible'})
+        for pdf in pdf_results:
+            if pdf.find("img", {
+                "src": "https://egela.ehu.eus/theme/image.php/fordson/core/1619589309/f/pdf"}):  # egelako elementuetatik, pdf bezala agertzen direnak bilatu
+                # ACTUALIZAR BARRA DE PROGRESO
+                # POR CADA PDF ANIADIDO EN self._refs
+                progress += progress_step
+                progress_var.set(progress)
+                progress_bar.update()
+                time.sleep(0.1)
 
-        for each in a_links:
-            if each['src'].find("/pdf") != -1:
-                print("\n##### PDF-a bat aurkitu da! #####")
-                pdf_link = each.parent['href']
-                uria = pdf_link
-                headers = {'Host': 'egela.ehu.eus', 'Cookie': self._cookie}
-                erantzuna = requests.get(uria, headers=headers, allow_redirects=False)
-                kodea = erantzuna.status_code
-                deskribapena = erantzuna.reason
-                print(str(kodea) + " " + deskribapena)
-                edukia = erantzuna.content
+                uri = pdf.find("a")["href"] + "&redirect=1"
+                metodo = 'POST'
+                datuak = ""
+                goiburuak = {'Host': 'egela.ehu.eus', 'Content-Type': 'application/x-www-form-urlencoded',
+                             'Content-Length': str(len(datuak)), "Cookie": self._cookie}
+                erantzuna = requests.request(metodo, uri, data=datuak, headers=goiburuak,
+                                             allow_redirects=False)
 
-                soup2 = BeautifulSoup(edukia, 'html.parser')
-                div_pdf = soup2.find('div', {'class': 'resourceworkaround'})
-                pdf_link = div_pdf.a['href']
-                pdf_izena = pdf_link.split('/')[-1]
-                self._refs.append({'link': pdf_link, 'pdf_name': pdf_izena})
+                pdf_uri = erantzuna.headers['Location']
+                pdf_link = pdf_uri.split("mod_resource/content/")[1].split("/")[1].replace("%20", "_")
+                self._refs.append({"pdf_name": pdf_link, "pdf_link": pdf_uri})
 
-            progress += 1.5
-            progress_var.set(progress)
-            progress_bar.update()
-            time.sleep(0.1)
-
+        for elem in self._refs:
+            print(elem)
         popup.destroy()
         return self._refs
 
     def get_pdf(self, selection):
         print("##### PDF-a deskargatzen... #####")
-        metodoa = 'GET'
-        uria = self._refs[selection]['link']
-        print(uria)
-        headers = {'Host': 'egela.ehu.eus','Cookie': self._cookie}
-        erantzuna = requests.get(uria, metodoa, headers=headers, allow_redirects=False)
-        pdf_file = erantzuna.content
         pdf_name = self._refs[selection]['pdf_name']
+        pdf_link = self._refs[selection]['pdf_link']
+
+
+        cook = self._cookie
+        goiburuak = {'Host': 'egela.ehu.eus', 'Content-Type': 'application/x-www-form-urlencoded',
+                     'Content-Length': '0', "Cookie": cook}
+        erantzuna = requests.request('GET', pdf_link, headers=goiburuak, allow_redirects=False)
+
+        pdf_file = erantzuna.content
+        print(pdf_name + " downloaded !")
 
         return pdf_name, pdf_file
 
